6277d28cc1f58f84f67db022411ed0e58a87b79c
bingoer <364350794@qq.com>
2018-03-29 00:55:24 +0800

    update Monetary.java and Coin.java

--- core/src/main/java/org/bitcoinj/core/Monetary.java
+++ core/src/main/java/org/bitcoinj/core/Monetary.java
@@ -26,13 +26,18 @@ public interface Monetary extends Serializable {
     /**
      * Returns the absolute value of exponent of the value of a "smallest unit" in scientific notation. For Bitcoin, a
      * satoshi is worth 1E-8 so this would be 8.
+     * 返回最小单元的指数，1e-8 就返回8
      */
-    int smallestUnitExponent();
+    int smallestUnitExponent()
 
     /**
      * Returns the number of "smallest units" of this monetary value. For Bitcoin, this would be the number of satoshis.
-     */
-    long getValue();
+     *
+     *
+	 */
+    //返回以satoshis为单位的值
 
-    int signum();
+     long getValue();
+
+	 int signum();
 }
